2abb908c1e8d378974b4fdc6ce527fcf465bdbc5
Rupankar Lahon <rupankarb1@gmail.com>
2025-06-27 20:45:53 +0530

    Delete asset-management-backend directory

--- asset-management-backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('assets_db', 'root', 'Riki@0376', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
